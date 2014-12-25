c46959b2158452f29ec6662604aee87485e13056
Rio Astamal <me@rioastamal.net>
2014-12-25 15:53:45 +0700

    Add initial wordpress main file

---
+++ wow-sphinx.php
@@ -0,0 +1,11 @@
+<?php defined('ABSPATH') or die("No script kiddies please!");
+/*
+Plugin Name: Wow Sphinx
+Plugin URI: https://github.com/wowrack/wow-sphinx
+Description: Wow Sphinx is a Wordpress plugin which allows Wordpress site to use Sphinx search server to enable blazing fast search for post, page or comments.
+Version: 1.0
+Author: Rio Astamal
+Author URI: http://rioastamal.net/
+License: MIT
+License URI: http://opensource.org/licenses/MIT
+*/
